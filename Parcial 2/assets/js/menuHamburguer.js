5fad4e9bffa3887632782e128c17f404342f8194
marr109 <88809862+marr109@users.noreply.github.com>
2023-06-24 22:43:46 -0400

    Eliminado

--- Parcial 2/assets/js/menuHamburguer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburgerBtn = document.getElementById("hamburger-btn");
-    const navbarMenu = document.getElementById("navbar-menu");
-  
-    hamburgerBtn.addEventListener("click", function () {
-      navbarMenu.classList.toggle("show");
-    });
-});
-  
